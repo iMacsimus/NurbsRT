b60c2d8427d1de7812f4457d2e38847ef5f6a4a9
Maksim Kunts <kuncz.maks@mail.ru>
2025-12-04 15:03:13 +0300

    Example of reparametrization

--- examples/example_vase.cpp
+++ examples/example_vase.cpp
@@ -6,7 +6,7 @@
 #include <nurbs_rt/nurbs_rt.h>
 
 int main() {
-  std::vector<nurbs_rt::float3> pointsFlatten = {
+  std::vector<LiteMath::float3> pointsFlatten = {
     {1.0000, 1.0000, 0.0000}, {1.0000, 1.0000, 1.0000}, {0.0000, 1.0000, 1.0000}, {-1.0000, 1.0000, 1.0000}, {-1.0000, 1.0000, 0.0000}, {-1.0000, 1.0000, -1.0000}, {0.0000, 1.0000, -1.0000}, {1.0000, 1.0000, -1.0000}, {1.0000, 1.0000, 0.0000},
     {0.1667, -0.3846, 0.0000}, {0.1667, -0.3846, 0.1667}, {0.0000, -0.3846, 0.1667}, {-0.1667, -0.3846, 0.1667}, {-0.1667, -0.3846, 0.0000}, {-0.1667, -0.3846, -0.1667}, {0.0000, -0.3846, -0.1667}, {0.1667, -0.3846, -0.1667}, {0.1667, -0.3846, 0.0000},
     {0.3333, -1.0000, 0.0000}, {0.3333, -1.0000, 0.3333}, {0.0000, -1.0000, 0.3333}, {-0.3333, -1.0000, 0.3333}, {-0.3333, -1.0000, 0.0000}, {-0.3333, -1.0000, -0.3333}, {0.0000, -1.0000, -0.3333}, {0.3333, -1.0000, -0.3333}, {0.3333, -1.0000, 0.0000}
@@ -16,12 +16,12 @@ int main() {
     1, 0.707107, 1, 0.707107, 1, 0.707107, 1, 0.707107, 1, 
     1, 0.707107, 1, 0.707107, 1, 0.707107, 1, 0.707107, 1
   };
-  std::vector<float> uKnots = {3, 3.5, 4};
+  std::vector<float> uKnots = {0, 0.5, 1};
   std::vector<uint32_t> uMults = {2, 1, 2};
   std::vector<float> vKnots = {0, 0.25, 0.5, 0.75, 1};
   std::vector<uint32_t> vMults = {3, 2, 2, 2, 3};
 
-  nurbs_rt::Matrix<nurbs_rt::float3> points(9, 3, pointsFlatten.data());
+  nurbs_rt::Matrix<LiteMath::float3> points(9, 3, pointsFlatten.data());
   nurbs_rt::Matrix<float> weights(9, 3, weightsFlatten.data());
 
   nurbs_rt::NurbsSurface surface(points, weights, uKnots, uMults, vKnots, vMults);
@@ -34,6 +34,7 @@ int main() {
 
   LiteMath::float4x4 transform = LiteMath::rotate4x4Z(3.14f/8);
   surface.transform(transform);
+  surface.reparametrizeU({3.0f, 8.0f});
 
   nurbs_rt::drawUniformSamples(surface, image, 1000, 1000, viewProj);  
   std::filesystem::path savePathUniformSamples = std::filesystem::current_path() / "uniform_samples.bmp";
